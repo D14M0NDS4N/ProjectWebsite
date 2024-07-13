2df4bcdeddbb3d4636dde733150a8b060a89c353
Arsyandi Nadhif Aljune <78140576+D14M0NDS4N@users.noreply.github.com>
2024-07-14 01:51:55 +0700

    Add files via upload

--- data-produk.php
+++ data-produk.php
@@ -34,35 +34,45 @@
     <!-- content -->
     <div class="section">
         <div class="container">
-            <h3>Data Kategori</h3>
+            <h3>Data Produk</h3>
             <div class="box">
-                <p><a href="tambah-kategori.php">Tambah Data</a></p>
+                <p><a href="tambah-produk.php">Tambah Data</a></p>
                 <table border="1" cellspacing="0" class="table">
                     <thead>
                         <tr>
                             <th width="60px">No</th>
-                            <th>Kategori</th>
+                            <th>Produk</th>
+                            <th>Nama Produk</th>
+                            <th>Harga</th>
+                            <th>Gambar</th>
+                            <th>Status</th>
                             <th width="150px">Aksi</th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php
                             $no = 1;
-                            $kategori = mysqli_query($conn, "SELECT * FROM `tb_category` ORDER BY `tb_category`.`category_id` DESC"); 
-                            if(mysqli_num_rows($kategori) > 0){
-                            while($row = mysqli_fetch_array($kategori)){  
+                            $produk = mysqli_query($conn, "SELECT * FROM `tb_product` LEFT JOIN tb_category USING (category_id) 
+                            ORDER BY `product_id` DESC");
+                            if(mysqli_num_rows($produk) > 0){
+                            while($row = mysqli_fetch_array($produk)){  
                         ?>
                         <tr>
                             <td><?php echo $no++ ?></td>
                             <td><?php echo $row['category_name'] ?></td>
+                            <td><?php echo $row['product_name'] ?></td>
+                            <td>Rp. <?php echo number_format($row['product_price']) ?></td>
+                            <td><a href="produk/<?php echo $row['product_image'] ?>" target="_blank"><img src="produk/<?php echo $row['product_image'] ?>" width="50px"></a></td>
+                            <td><?php echo ($row['product_status'] == 0)? 'Tidak Aktif':'Aktif'; ?></td>
                             <td>
-                                <a href="edit-kategori.php?id=<?php echo $row['category_id'] ?>">Edit</a> || <a href="proses-hapus.php?idk=<?php echo $row['category_id'] ?>" onclick="return confirm('Apakah Anda Yakin Ingin Hapus?')">Hapus</a>
+                                <a href="edit-produk.php?id=<?php echo $row['product_id'] ?>">Edit</a> || <a href="proses-hapus.php?idp=
+                                <?php echo $row['product_id'] ?>" onclick="return confirm('Apakah Anda Yakin Ingin Hapus?')">Hapus</a>
                             </td>
                         </tr>
                         <?php }}else{ ?>
                             <tr>
-                                <td colspan="3">Tidak ada data</td>
-                            </tr>
+                                <td colspan="7">Tidak ada data</td>
+                            <tr>
                         <?php } ?>
                     </tbody>
                 </table>
